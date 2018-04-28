48ea1ab990e32ff2854ba79617464fc644ba5aeb
George Yoo <georgeyoo@Georges-MBP-2.lan>
2018-04-28 07:23:53 -0400

    Working MVP

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -3,19 +3,8 @@ var proxyURL = "https://shielded-hamlet-43668.herokuapp.com/";
 let queryURL = "https://api.yelp.com/v3/businesses/search?term=japanese&location=";
 let locationString;
 let radiusString;
-const apiKey = "nRFpFFodXZb24Nyj43Jm52pW5I0A-Wr1-OIh5qYV0er3_5y5TymJViOTGw-Mkam10P_rTdz5BmElL6gyXFA-0EIQMjiEpPDiOx8TEtmWsJd-Amvn8kSE5QAdYj_kWnYx";
-const yelpHeader = {
-        Authorization: "Bearer" + apiKey,
-        "x-requested-with": "xhr" 
-    }
+let apiKey = "nRFpFFodXZb24Nyj43Jm52pW5I0A-Wr1-OIh5qYV0er3_5y5TymJViOTGw-Mkam10P_rTdz5BmElL6gyXFA-0EIQMjiEpPDiOx8TEtmWsJd-Amvn8kSE5QAdYj_kWnYx";
 let queryRadius;
-
-// CORS workaround
-// jQuery.ajaxPrefilter(function(options) {
-//     if (options.crossDomain && jQuery.support.cors) {
-//         options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
-//     }
-// });
 
 // Giphy API
 const giphyURL = "https://api.giphy.com/v1/gifs/search?q=";
@@ -49,7 +38,7 @@ function giphy() {
             // console.log(req.data[0].images.original.url);
             let image = req.data[newRandNum].images.original.url;
             
-            let htmlStuff = "<div class='gif-image'>"
+            let giphyHTML = "<div class='gif-image'>"
                             +"<img src=" 
                             + '"' 
                             + image 
@@ -57,7 +46,7 @@ function giphy() {
                             + ">"
                             + "</div>";
 
-            $(".random-gif-" + i).append(htmlStuff);
+            $(".random-gif-" + i).append(giphyHTML);
         });
     }
 }
@@ -67,7 +56,7 @@ function buttonClick() {
     $("#search-btn").on("click", function() {
         event.preventDefault();
         let locationValue = $("#location-input").val();
-        console.log(locationValue);
+        // console.log(locationValue);
         yelpCall(locationValue);
     });
 }
@@ -75,17 +64,46 @@ function buttonClick() {
 
 // Insert ajax call hereop
 
-// yelp API?
+// yelp API
 function yelpCall(location) {
+    console.log(location);
     $.ajax({
         method: "GET",
         "crossDomain": true,
-        headers: yelpHeader,
         "async": true,
+        headers: {
+            "Authorization": "Bearer" + " " + apiKey
+        },
         url: proxyURL + queryURL + location
     }).then(function(res, req) {
-        console.log(req)
+        // console.log(res)
+        renderYelp(res)
+
+        let resultBoarder = '<div class="result-border">' 
+                          + "</div>";
+        $(".yelp-results").append(resultBoarder);
     })
 };
+
+
+// Render Yelp Results!
+function renderYelp(res) {
+
+    for(let x = 0; x < 4; x++) {
+        console.log(res);
+        console.log(res.businesses[x].name)
+        let yelpImg = res.businesses[x].image_url;
+        let yelpHTML = "<div>"
+                     + '<img class="yelp-result-img" src=' 
+                     + '"' 
+                     + yelpImg 
+                     + '"' 
+                     + ">"
+                     + "</div>";
+
+        $(".grid-item-" + x).append(yelpHTML);
+    }
+}
+
 
 
